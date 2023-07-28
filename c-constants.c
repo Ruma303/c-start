724a99f204cfa6721db5dc3af7387a84f24fb6c4
Ruma303 <matteoh8@hotmail.it>
2023-07-29 00:27:07 +0200

    aggiunte variabili e costanti

---
+++ c-constants.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    const int CONST_1 = 50; //* Dichiarazione costante
+
+    printf();
+    return 0;
+
+}
---
+++ c-variabiles.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int my_var1; //* Dichiarazione
+
+    int var1, var2, var3; //* Dichiarazione multiple
+
+    int my_var2 = 100; //* Inizializzazione
+
+    int var4 = 4, var5 = 5, var6 = 6;  //* Inizializzazione multiple
+
+    var1 = 42;
+
+    //printf("%d", my_var1);
+    // 2867012
+
+    //printf("%d", var1); // 42
+
+    //$ Tipi di valori assegnabili alle variabili
+
+    int Var1 = 100; //* Literal
+    int Var2 = 100 / 25; //* Espressioni
+    int Var3 = somma(5, 5); //* Invocazioni
+
+    return 0;
+
+}
