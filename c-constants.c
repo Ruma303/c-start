5edfb3c60c9fc9485fc583dce3008eb677422d90
Ruma303 <matteoh8@hotmail.it>
2023-07-31 21:37:54 +0200

    completamento

--- c-constants.c
+++ c-constants.c
@@ -2,7 +2,9 @@
 int main()
 {
     const int CONST_1 = 50; //* Dichiarazione costante
+    #define PI 3.14; //* Dichiarare costante usando direttiva #define
 
+    const double pi = 3.14; //* Dichiarazione costante usando const
     printf();
     return 0;
 
--- c-variabiles.c
+++ c-variabiles.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main()
-{
+
+//$ Ambito delle variabili, fuori dal main()
+int var_globale = 20; //* Variabile globale
+void funzioneGlobale() {
+    int var_locale = 10; //* Variabile locale
+    printf("%d", var_globale); //? Variabile globale accessibile
+    printf("%d", var_locale); //? Variabile locale accessibile
+}
+
+int main(){
+
     int my_var1; //* Dichiarazione
 
     int var1, var2, var3; //* Dichiarazione multiple
@@ -20,8 +29,16 @@ int main()
 
     int Var1 = 100; //* Literal
     int Var2 = 100 / 25; //* Espressioni
-    int Var3 = somma(5, 5); //* Invocazioni
+    //int Var3 = somma(5, 5); //* Invocazioni
+
+
+    //$ Ambito delle variabili, all'interno di main()
+    printf("%d", var_globale); //? Variabile globale accessibile
+    //printf("%d", var_locale); //. Variabile locale non accessibile
+    funzioneGlobale();
 
     return 0;
 
 }
+
+
