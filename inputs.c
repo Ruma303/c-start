a462a8e2b0a1c5319d3acde1c37483a0c6b3f9fa
Ruma <matteoh8@hotmail.it>
2023-08-14 21:46:54 +0200

    aggiornamenti

--- inputs.c
+++ inputs.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
-    //$ getchar()
 
 int main() {
-    char c = getchar();
-    printf("%c: %d\n", c, c);
-    getchar();
+    //$ getchar()
+    /* char c = getchar();
+    printf("%c: %d\n", c, c); */
+
+    /* puts("Ciao");
+    getchar(); */
+
+    //$ scanf()
+   /*  int x, y;
+    puts("Inserire primo valore: ");
+    scanf("%i",&x);
+    puts("Inserire secondo valore: ");
+    scanf("%i",&y);
+    int somma = x + y;
+    printf("\n%s %i\n", "Somma: ", somma); */
+
+    int x, y, num;
+    puts("Inserire due valori (separati da uno spazio): ");
+    num = scanf("%i %i", &x, &y);
+
+    int somma = x + y;
+    printf("\n%s %i\n", "Somma :", somma);
+    printf("Numeri di variabili acquisite: %i\n", num);
+
+
     return 0;
 }
+
 
